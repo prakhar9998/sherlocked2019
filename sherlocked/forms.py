3b7f045be736460b593c21f00ff7728b7926ee94
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-02-12 22:46:13 +0530

    add forms

---
+++ sherlocked/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widgets=forms.PasswordInput)
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username',  'email')
