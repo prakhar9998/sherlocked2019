64e4634de559f3ffbc2c0b37c7af627f991b6b99
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-02-14 11:11:49 +0530

    add play method

--- sherlocked/urls.py
+++ sherlocked/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('play/', views.play, name='play'),
 ]
--- sherlocked/views.py
+++ sherlocked/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .forms import PlayerCreationForm
-from .models import Player
+from .models import Player, Question
 
 class SignUp(generic.CreateView):
     form_class = PlayerCreationForm
@@ -18,4 +18,33 @@ def dashboard(request):
 
 @login_required
 def play(request):
-    pass
+    # user submitted an answer to a question
+    player = Player.objects.get(username=request.user.username)
+    question = Question.objects.get(question_level=player.level)
+    
+    if request.method=='POST':
+        print("answer to question is: " + question.answer.lower())
+        print("submitted answer is: " + str(request.POST.get("answer")))
+
+        if question.answer.lower() == str(request.POST.get("answer")):
+            # TODO: Add a check for winning condition
+            # Increment the level of player if last question is not reached
+            player.level = player.level + 1
+            player.save()
+
+            # fetch next question to display if answer is correct
+            question = Question.objects.get(question_level=player.level)
+
+    question_text = question.question_text
+    question_story = question.question_story
+    question_level = question.question_level
+
+    return render(
+        request,
+        'sherlocked/play.html',
+        {
+            'question_text': question_text,
+            'question_story': question_story,
+            'question_level': question_level,
+        }
+    )
